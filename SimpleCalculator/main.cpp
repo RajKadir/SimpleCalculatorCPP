47d565ee4955ce1721b04b3a497f3d6da219104e
Raj Kadir <rajkadir@hotmail.co.uk>
2016-02-11 20:52:55 +0000

    Added function which adds two numbers together

--- SimpleCalculator/main.cpp
+++ SimpleCalculator/main.cpp
@@ -2,8 +2,32 @@
 
 using namespace std;
 
+// function which adds two numbers together
+int addNumbers(int one, int two){
+    return one + two;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    // local variables
+    int numOne;
+    int numTwo;
+    int sum;
+
+    // Ask user prompts
+    cout << "Welcome to this simple calculator, please enter a number" << endl;
+    cin >> numOne;
+    cout << "The number entered was: " << numOne << endl;
+    cout << "Now enter the number you wish to add to this: " << endl;
+    cin >> numTwo;
+    cout << "The 2nd number entered was: " << numTwo << endl;
+    // call a function which calculates the sum
+    sum = addNumbers(numOne, numTwo);
+    cout << "The value of both of these added together: " << sum << endl;
+
+    // End
     return 0;
 }
+
+
+
